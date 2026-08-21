ffd7aef4fae9a8a1d644874c2acfd5b9012cde09
Your Name <you@example.com>
2026-08-22 00:20:54 +0700

    payment later

--- src/main/java/kh/edu/istad/ite/features/cart/dto/StorefrontCheckoutRequest.java
+++ src/main/java/kh/edu/istad/ite/features/cart/dto/StorefrontCheckoutRequest.java
@@ -1,12 +1,14 @@
 package kh.edu.istad.ite.features.cart.dto;
 
 import jakarta.validation.constraints.NotNull;
+import kh.edu.istad.ite.shared.enums.PaymentMethodType;
 
 import java.util.UUID;
 
 
 public record StorefrontCheckoutRequest(
         @NotNull UUID businessId,
-        String note
+        String note,
+        PaymentMethodType paymentMethod
 ) {
 }
